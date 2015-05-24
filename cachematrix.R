7068d3c41d767c260c167330750902ad189a2003
Ken Richlin <krichlin@gmail.com>
2015-05-24 01:11:13 -0400

    First commit
    Just the stubs with some pseudocode and comments.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,33 @@
 
 ## Write a short comment describing this function
 
+## makeCacheMatrix is a function used in Programming Assignment 2
+## This function accepts a matrix as an input, and 
+
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+
+
+
+
+
+## This should really go into its own separate file, but let's leave it here for the moment
+## cacheSolve is a function that "solves" a matrix, ie, mathematically inverts the matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+     
+     
+  ## A matrix needs to be square to be inverted.  The first thing we should do is to check this.
+  ## However the assignment doesn't require it, so I've left this part commented out.  
+     
+  ##   is_square <- ## is it square?  save result as logical     
+  ##   if !(is_square) { throw an error }
+  ##   else if {   ## otherwise go through with the calculation to determine the inverse. }
+         
+         
+     
 }
